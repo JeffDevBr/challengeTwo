d129f45bf0669c6e552c37c55aecf5fdc18a7bd5
Jefferson <jeff.dev.br@gmail.com>
2024-06-27 20:31:31 -0300

    ✔  Resolvido 03: Fashcards

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,42 @@
+const cardsInfo = [
+    {
+        id: 7336,
+        question: "Do quê aplicações React são feitas?",
+        answer: "Componentes",
+    },
+    {
+        id: 8832,
+        question: "Qual é o nome da sintaxe usada para descrever UI no React?",
+        answer: "JSX",
+    },
+    {
+        id: 3457,
+        question: "Em qual linguagem o React é baseado?",
+        answer: "JavaScript",
+    },
+    {
+        id: 9103,
+        question: "Qual método usar para renderizar uma lista no React?",
+        answer: "map",
+    },
+    {
+        id: 1297,
+        question: "Qual empresa criou o React?",
+        answer: "Meta",
+    },
+    {
+        id: 2002,
+        question: "Como passar informações para um componente React?",
+        answer: "Através de props",
+    },
+]
 const App = () => {
-    const handleClickToggle = (e) => console.log(e.target.textContent)
-    const handleClickPrevious = (e) => console.log(e.target.textContent)
-    const handleClickNext = (e) => console.log(e.target.textContent)
+    const handleClick = (id) => console.log(id)
     return (
-        <>
-            <div className="container-close">
-                <button onClick={handleClickToggle} className="close">Fechar</button>
-            </div>
-
-            <div className="steps">
-                <div className="numbers">
-                    <div className="active">1</div>
-                    <div>2</div>
-                    <div>3</div>
-                </div>
-
-                <p className="message">Passo 1: Entender o problema do cliente</p>
-
-                <div className="buttons">
-                    <button onClick={handleClickPrevious} >Anterios</button>
-                    <button onClick={handleClickNext}>Próximo</button>
-                </div>
-            </div>
-        </>
+        <ul className="flashcards">
+            {cardsInfo.map(({ id, question }) =>
+                <li onClick={() => handleClick(id)} className="card" key={id}> {question}</li>)}
+        </ul>
     )
 }
 
